775c3f257f83c5910db5faf169cefd6b2e70b7de
Ryu <zoelabbb@gmail.com>
2023-12-26 14:23:17 +0700

    Update DB Migration

--- database/migrations/2023_12_25_081252_create_gejalas_table.php
+++ database/migrations/2023_12_25_081252_create_gejalas_table.php
@@ -1,29 +1,22 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateGejalasTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('gejalas', function (Blueprint $table) {
             $table->id();
-            $table->string('Kode_Gejala');
+            $table->string('Kode_Gejala')->unique();
             $table->string('Nama_Gejala');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('gejalas');
     }
-};
+}
--- database/migrations/2023_12_25_114636_create_aturans_table.php
+++ database/migrations/2023_12_25_114636_create_aturans_table.php
@@ -1,35 +1,27 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateAturansTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('aturans', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('Kode_Gejala');
-            $table->unsignedBigInteger('');
-            // Tambahkan kolom lain sesuai kebutuhan
+            $table->unsignedBigInteger('Kode_Penyakit');
             $table->timestamps();
         });
 
         Schema::table('aturans', function (Blueprint $table) {
-            $table->foreign('Kode_Gejala')->references('Kode_Gejala')->on('gejalas')->onDelete('cascade');
-            $table->foreign('Kode_Penyakit')->references('Kode_Penyakit')->on('penyakits')->onDelete('cascade');
+            $table->foreign('Kode_Gejala')->references('id')->on('gejalas')->onDelete('cascade');
+            $table->foreign('Kode_Penyakit')->references('id')->on('penyakits')->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('aturans');
     }
-};
+}
